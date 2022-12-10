340ebc5a56b977747d2b0f137926c5253ae59595
Dmitry Lomanchuk <daloman@gmail.com>
2022-12-10 18:17:40 +0300

    Add http.Client Timeout

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -2,24 +2,24 @@ package connector
 
 import (
 	"net/http"
+	"net/url"
 	"time"
 )
 
 // Use fromEnv
 // Const is for test purposes only
-//const proxyUrl = "http://192.168.1.100:3128"
+const proxyUrl = "http://192.168.1.100:3128"
 
 // Init Client connection over httpProxy
 func NewConnection() *http.Client {
-	//proxy, _ := url.Parse(proxyUrl)
+	proxy, _ := url.Parse(proxyUrl)
 
 	tr := &http.Transport{
-		MaxIdleConns:    10,
-		IdleConnTimeout: 30 * time.Second,
-
+		MaxIdleConns:       10,
+		IdleConnTimeout:    30 * time.Second,
 		DisableCompression: true,
-		//Proxy:              http.ProxyURL(proxy),
+		Proxy:              http.ProxyURL(proxy),
 	}
-	conn := &http.Client{Transport: tr}
+	conn := &http.Client{Transport: tr, Timeout: 10 * time.Second}
 	return conn
 }
